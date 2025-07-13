f55c90e42a84bf22bea3b7f88d2ef477a2539d50
DC <63706443+codieg0@users.noreply.github.com>
2025-07-13 23:10:00 +0100

    flipping issue resolved

--- get_users.sh
+++ get_users.sh
@@ -4,36 +4,38 @@ echo
 read -p "Enter domain: " domain
 read -p "Enter stack: " stack
 read -p "Enter username: " user
-read -s -p "Enter Password: " password
-echo
+read -sp "Enter Password: " password
 echo
 read -p "Choose user type (end_user / silent_user / organization_admin / all): " usertype
 echo
 
-# Make the API call and filter with jq
-curl -s -X GET -H "X-User: $user" -H "X-Password: $password" \
-    "https://$stack.proofpointessentials.com/api/v1/orgs/$domain/users" | jq -r --arg t "$usertype" '
+# Store jq filter in a variable (or use heredoc)
+jq_filter=$(cat <<'EOF'
+  def print_all($all):
+    "There are \($all|length) active users.\n\n" +
+    "\($all|map(select(.type == "organization_admin"))|length) Org. Admins\n" +
+    "\($all|map(select(.type == "end_user"))|length) End Users\n" +
+    "\($all|map(select(.type == "silent_user"))|length) Silent Users\n\n" +
+    "Org. Admins\n" +
+    ($all|map(select(.type == "organization_admin")|.primary_email)|join("\n")) + "\n\n" +
+    "End Users\n" +
+    ($all|map(select(.type == "end_user")|.primary_email)|join("\n")) + "\n\n" +
+    "Silent Users\n" +
+    ($all|map(select(.type == "silent_user")|.primary_email)|join("\n"));
+
   if .users == null then
     "No users found or invalid credentials."
   elif $t == "all" then
-    ( [ .users[] | select(.is_active==true) ] as $all
-    | "There are \($all|length) active users.\n\n"
-      + "\($all|map(select(.type == \"organization_admin\"))|length) Org. Admins\n"
-      + "\($all|map(select(.type == \"end_user\"))|length) End Users\n"
-      + "\($all|map(select(.type == \"silent_user\"))|length) Silent Users\n\n"
-      + "Org. Admins\n"
-      + ($all|map(select(.type == \"organization_admin\")|.primary_email) | join("\n"))
-      + "\n\n"
-      + "End Users\n"
-      + ($all|map(select(.type == \"end_user\")|.primary_email) | join("\n"))
-      + "\n\n"
-      + "Silent Users\n"
-      + ($all|map(select(.type == \"silent_user\")|.primary_email) | join("\n"))
-    )
+    ([.users[] | select(.is_active==true)] | print_all(.))
   else
-    ( [ .users[] | select(.type==$t and .is_active==true) ] as $filtered
-    | "There are \($filtered|length) active \($t) users.\n\n"
-      + ($filtered|map(.primary_email) | join("\n"))
-    )
+    ([.users[] | select(.type==$t and .is_active==true)] as $f |
+     "There are \($f|length) active \($t) users.\n\n" +
+     ($f|map(.primary_email)|join("\n")))
   end
-'
+EOF
+)
+
+# Run curl and jq
+curl -s -H "X-User: $user" -H "X-Password: $password" \
+  "https://$stack.proofpointessentials.com/api/v1/orgs/$domain/users" |
+jq -r --arg t "$usertype" "$jq_filter"
